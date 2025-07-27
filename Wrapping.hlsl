2d1a1bdacc60a6bb474b4153bc5f0868060dbbb5
sammedhi <sammedhi2@hotmail.fr>
2025-07-27 15:57:13 +0200

    Now the WorldToCylinder function use the cylindreRadius parameter instead of the z positio of the vertex so the actual shader can control which value to pass into

--- Wrapping.hlsl
+++ Wrapping.hlsl
@@ -11,8 +11,7 @@ float4 WorldToCylinder(float4 worldPoint, float cylinderRadius, float gameWidth)
 {
     float percent = worldPoint.x / (gameWidth / 2);
     float angle = percent * PI ;
-    float radius = worldPoint.z;
-    float2 circularCoord = float2(sin(angle), cos(angle)) * radius;
+    float2 circularCoord = float2(sin(angle), cos(angle)) * cylinderRadius;
     float4 cylinderVertex = float4(circularCoord.x,worldPoint.y, circularCoord.y, 1);
     return cylinderVertex;
 }
